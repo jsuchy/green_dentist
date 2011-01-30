374c23bb83834cde4f21158d35e5e22412e5654b
Jim Suchy <jsuchy@gmail.com>
2011-01-29 20:35:45 -0600

    skeleton rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GreenDentist::Application.config.secret_token = '73c2566b05db09f4609d963ad483cfd971791ab6019eaf5e87a89eadc38526b7540e5097dc1083ba8be41e26d5eddaa463719e4ccf547e814686fca505c1f016'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GreenDentist::Application.config.session_store :cookie_store, :key => '_green_dentist_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GreenDentist::Application.config.session_store :active_record_store
