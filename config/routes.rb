1780b6efecd0bf6c4cc5732f10d6561bedb6707c
Jim Suchy <jsuchy@gmail.com>
2011-02-13 15:01:23 -0600

    redesign

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,6 @@ GreenDentist::Application.routes.draw do
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
-  match 'products' => 'main#products'
-  match 'company' => 'main#company'
-  match 'links' => 'main#links'
-  match 'contact' => 'main#contact'
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
---
+++ public/javascripts/cufon-replace.js
@@ -0,0 +1,4 @@
+Cufon.replace('#menu li a, #menu_active', { fontFamily: 'Arial-Relcom', hover:true });
+Cufon.replace('.h1', { fontFamily: 'Cricket', textShadow:'#32391b 1px 2px' });
+Cufon.replace('h1, #welcome a', { fontFamily: 'Cricket-Light', hover:true });
+Cufon.replace('#welcome p', { fontFamily: 'Myriad Pro' });
